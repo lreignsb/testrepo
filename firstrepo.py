527166a2fb2a12d314a96a58bc787e18cd716808
lreignsb <68592310+lreignsb@users.noreply.github.com>
2020-07-24 17:11:58 +0800

    Add files via upload

---
+++ firstrepo.py
@@ -0,0 +1,12 @@
+Python 3.8.4 (v3.8.4:dfa645a65e, Jul 13 2020, 10:45:06) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("This is just a test python file for my first GitHub repository :)")
+This is just a test python file for my first GitHub repository :)
+>>> print ("As with every first programming language I learn, I guess I should start it with the so called "Hello World"")
+SyntaxError: invalid syntax
+>>> print ("As with every first programming language I learn, I guess I should start it with the so called Hello World Line")
+As with every first programming language I learn, I guess I should start it with the so called Hello World Line
+>>> print ("Hello World")
+Hello World
+>>> 
